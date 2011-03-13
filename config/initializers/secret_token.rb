7ddab312209dad5d40800b185c75e303ebf900a1
choonkeat <choonkeat@gmail.com>
2011-03-13 16:04:05 +0800

    rails3 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deliveryboy::Application.config.secret_token = 'c91e18b1e5199d373f623b16ecd5f31319a534ea670c5da82b0c628330028ed74efe990535a5bf5ccfdef044899673b8bbf04db7699fdf47e6c0181ef6fc987b'
