baf6c004d657fc86c715c73e49d16a743edc1b8f
choonkeat <choonkeat@gmail.com>
2011-03-28 19:34:09 +0800

    return-path config for plugins/mta

--- lib/deliveryboy/plugins/mta.rb
+++ lib/deliveryboy/plugins/mta.rb
@@ -17,12 +17,13 @@ class Deliveryboy::Plugins::Mta
   def initialize(config)
     @klass, @method = (config[:invoke] || 'Mail::SMTP#deliver!').split('#')
     @agent = @klass.constantize.new(config.except(:invoke))
+    @returnpath = config[:returnpath]
   end
 
   def handle(mail, recipient)
     mail.extend(ModifiableDestinations)
     mail.destinations = [recipient]
-    # mail.return_path = "donotreply@#{`hostname`.strip}"
+    mail.return_path = @returnpath || mail.sender || mail.from_addrs.first
     start = Time.now.to_f
     @agent.send(@method, mail)
     logger.debug "[mta] sent to #{mail.destinations.join(',')} via #{@klass} in #{Time.now.to_f - start}s"
